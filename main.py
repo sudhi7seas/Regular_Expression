35e9f1562503712822362e339fb8cb197d8b80e7
sudhi777 <87425107+sudhi7seas@users.noreply.github.com>
2023-01-31 06:23:21 +0000

    Added new logic that returns all the list items that contain Delhi and an email address.

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-# Write a regular expression that returns all the list items that contain the word # Delhi. The list is stored in the data variable:
-import re
+#Write a regular expression that returns all the list items that contain Delhi and an email address.
 
 data=[
     "mr Jim Cloudy, Texas, 01091231, 1 dog 1 cat, jim.cloudy@example.com", 
-   "mrs Anna Cloudy, Delhi, 2dogs 1fish bathlover@example.com",
+    "mrs Anna Cloudy, Delhi, 2dogs 1fish bathlover@example.com",
     "Mrs. Sarah Prost, Baghdad, +4327629101, 1 hamster, 2 crocodiles",
     "Ms Beta Palm Ontario 08234211 12 cats, beta@example.com",
     "mr. Dog Bells texas 09234211 3 honey badgers alta_bells.example.com",
@@ -11,17 +10,22 @@ data=[
     "mrs Alma Stills Delhi 01231981 1 dog",
     "mr Sen Kumar Delhi 3456 ants"
 ]
-#Your code solution output the following:
+#Your solution should output the following:
 
-#['mrs Anna Cloudy, Delhi, 2dogs 1fish bathlover@example.com',
-# 'mrs Alma Stills Delhi 01231981 1 dog',
-# 'mr Sen Kumar Delhi 3456 ants']
+#['mrs Anna Cloudy, Delhi, 2dogs 1fish bathlover@example.com']
 
-pattern = re.compile(".*Delhi.*", re.IGNORECASE)
+import re
+
+pattern = re.compile(".*Delhi.*[^ ]+@[^ ]+\.[a-z]+", re.IGNORECASE)
+
 matches = [match for match in data if pattern.findall(match)]
+
 print(matches)
 
-#.*Delhi.* instructs the program that Delhi could be found between any number of 
-#characters. The part .* means there could be any number of characters before or 
-#after Delhi. The expression .*Delhi.* could also be replaced with just Delhi and 
-#still produce the same result, but .*Delhi.* is more explicit and more logical.
+# .*Delhi.* searches for Delhi anywhere in the line.
+
+# [^ ]+ searches for one or more characters other than white space.
+
+# @[^ ]+ searches for an @ symbol followed by any number of characters other than space.
+
+# \.[a-z]+ searches for a dot followed by any number of letters.
